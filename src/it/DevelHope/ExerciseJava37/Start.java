1da852edead0374ea0b58b72fee15cc5d2ad334e
Marco Lo Bello <xSalamander@outlook.it>
2022-12-20 16:44:46 +0100

    Initial Commit

--- src/it/DevelHope/ExerciseJava37/Start.java
+++ src/it/DevelHope/ExerciseJava37/Start.java
@@ -7,22 +7,18 @@ public class Start {
     public static void main(String[] args) {
 
         LocalDate todayDate = LocalDate.now();
+
         int currentDayOfTheMonth = todayDate.getDayOfMonth(); // GIORNO ATTUALE DEL MESE
         int lengthOfTheMonth = todayDate.lengthOfMonth();     // GIORNI TOTALI DEL MESE
         int daysUntilTheEndOfTheMonth = lengthOfTheMonth - currentDayOfTheMonth; //RISULTATO DEI GIORNI MANCANTI ALLA FINE DEL MESE
 
+        int currentDayOfTheYear = todayDate.getDayOfYear(); // GIORNO ATTUALE DELL'ANNO
+        int lengthOfTheYear = todayDate.lengthOfYear();     // GIORNI TOTALI DELL'ANNO
+        int daysUntilTheEndOfTheYear = lengthOfTheYear - currentDayOfTheYear; // RISULTATO DEI GIORNI MANCANTI ALLA FINE DELL'ANNO
+
         System.out.println("There are " + daysUntilTheEndOfTheMonth + " days until the end of the Month.");
         System.out.println(" ------------------------------------------ ");
-
-        int currentDayOfTheYear = todayDate.getDayOfYear();
-        int lengthOfTheYear = todayDate.lengthOfYear();
-        int daysUntilTheEndOfTheYear = lengthOfTheYear - currentDayOfTheYear;
-
         System.out.println("The are " + daysUntilTheEndOfTheYear + " days until the end of the Year.");
-
-
-
-
     }
 }
 /* using LocaleDate, calculate and print:
